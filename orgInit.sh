2d05a03efbb795740335e0a3d518441401fb956f
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-13 00:57:25 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
-sfdx shane:org:create -f config/project-scratch-def.json -s -d 1 --userprefix blitz --userdomain back.log
-sfdx force:source:push
-sfdx force:user:permset:assign -n VolunteeringApp
-sfdx force:data:tree:import -p data/masterImportPlan.json
-sfdx force:apex:execute -f scripts/setup.cls
-sfdx shane:user:password:set -p sfdx1234 -g User -l User
-sfdx force:org:open
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p blitz -e back.log
+sf project deploy start
+sf demoutil user password set -p salesforce1 -g User -l User
+sf org assign permset -n VolunteeringApp
+sf data import tree -p data/masterImportPlan.json
+sf apex run -f scripts/setup.cls
+sf org open
